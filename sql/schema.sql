172ac9ba4970661363126b0618af5b184cccfefb
Ezequiel Arangue <eze.arangue@gmail.com>
2020-11-07 22:33:38 -0300

    Fix schema.sql

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,47 +1,119 @@
--- DROP DATABASE IF EXISTS football;
-CREATE DATABASE IF NOT EXISTS football
-  CHARACTER SET utf8mb4;
+CREATE DATABASE  IF NOT EXISTS `football` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `football`;
+-- MySQL dump 10.13  Distrib 8.0.22, for Linux (x86_64)
+--
+-- Host: 127.0.0.1    Database: football
+-- ------------------------------------------------------
+-- Server version	8.0.22
 
-USE football;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-CREATE TABLE IF NOT EXISTS competitions (
-  id        INT NOT NULL PRIMARY KEY,
-  name      VARCHAR(255),
-  code      VARCHAR(3) NOT NULL,
-  areaName  VARCHAR(255)
-);
+--
+-- Table structure for table `competitions`
+--
 
-CREATE TABLE IF NOT EXISTS teams (
-  id        INT NOT NULL PRIMARY KEY,
-  name      VARCHAR(255),
-  tla       VARCHAR(255),
-  shortName VARCHAR(255),
-  areaName  VARCHAR(255),
-  email     VARCHAR(255) DEFAULT NULL
-);
+DROP TABLE IF EXISTS `competitions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `competitions` (
+  `id` int NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `code` varchar(3) NOT NULL,
+  `areaName` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE TABLE IF NOT EXISTS players (
-  id              INT NOT NULL PRIMARY KEY,
-  name            VARCHAR(255),
-  position        VARCHAR(255) DEFAULT NULL,
-  dateOfBirth     DATE DEFAULT NULL,
-  countryOfBirth  VARCHAR(255),
-  nationality     VARCHAR(255) 
-) ;
+--
+-- Table structure for table `competitions_teams`
+--
 
-CREATE TABLE IF NOT EXISTS competitions_teams (
-  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  competition_id  INT NOT NULL,
-  team_id        INT NOT NULL,
-  CONSTRAINT fk_competition_comp_teams FOREIGN KEY (competition_id) REFERENCES competitions(id),
-  CONSTRAINT fk_team_comp_teams FOREIGN KEY (team_id) REFERENCES teams(id)
-);
+DROP TABLE IF EXISTS `competitions_teams`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `competitions_teams` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `competition_id` int NOT NULL,
+  `team_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_competition_comp_teams` (`competition_id`),
+  KEY `fk_team_comp_teams` (`team_id`),
+  CONSTRAINT `fk_competition_comp_teams` FOREIGN KEY (`competition_id`) REFERENCES `competitions` (`id`),
+  CONSTRAINT `fk_team_comp_teams` FOREIGN KEY (`team_id`) REFERENCES `teams` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1920 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Table structure for table `players`
+--
 
-CREATE TABLE IF NOT EXISTS team_players (
-  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  team_id     INT NOT NULL,
-  player_id   INT NOT NULL, 
-  CONSTRAINT fk_team_team_players FOREIGN KEY (team_id) REFERENCES teams(id),
-  CONSTRAINT fk_players_team_players FOREIGN KEY (player_id) REFERENCES players(id)
-)
+DROP TABLE IF EXISTS `players`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `players` (
+  `id` int NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `position` varchar(255) DEFAULT NULL,
+  `dateOfBirth` date DEFAULT NULL,
+  `countryOfBirth` varchar(255) DEFAULT NULL,
+  `nationality` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `teams`
+--
+
+DROP TABLE IF EXISTS `teams`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `teams` (
+  `id` int NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `tla` varchar(255) DEFAULT NULL,
+  `shortName` varchar(255) DEFAULT NULL,
+  `areaName` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `teams_players`
+--
+
+DROP TABLE IF EXISTS `teams_players`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `teams_players` (
+  `team_id` int NOT NULL,
+  `player_id` int NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`),
+  KEY `fk_team_team_players` (`team_id`),
+  KEY `fk_players_team_players` (`player_id`),
+  CONSTRAINT `fk_players_team_players` FOREIGN KEY (`player_id`) REFERENCES `players` (`id`),
+  CONSTRAINT `fk_team_team_players` FOREIGN KEY (`team_id`) REFERENCES `teams` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=70562 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed

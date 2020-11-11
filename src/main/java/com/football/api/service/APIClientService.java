85200e28adb6801524a3850ee2d7ead11ce09d2c
Ezequiel Arangue <eze.arangue@gmail.com>
2020-11-11 03:47:30 -0300

    Changes in APIClientService.java

--- src/main/java/com/football/api/service/APIClientService.java
+++ src/main/java/com/football/api/service/APIClientService.java
@@ -1,77 +1,86 @@
 package com.football.api.service;
 
-import spark.*;
-
 import com.football.api.exception.NotFoundException;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
 import java.io.IOException;
-import java.net.*;
-import java.net.http.*;
 
 public class APIClientService {
     
     private static final String HOST = "https://api.football-data.org";
     private static final String API_KEY = "bcad86d274ae489ab890992653d3d929";
-
+    
     public static JsonObject getLeagueFromApi(String league) throws IOException, InterruptedException, NotFoundException {
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-        .uri(URI.create(HOST+"/v2/competitions/"+league))
-        .header("X-Auth-Token", API_KEY).build();
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        OkHttpClient client = new OkHttpClient();
+        String url = HOST+"/v2/competitions/"+league;
+        Request request = new Request.Builder()
+        .url(url)
+        .header("X-Auth-Token", API_KEY)
+        .build();
+        Response response = client.newCall(request).execute();
         
+        if (response.code() == 404)
+            throw new NotFoundException(404, "Not found", null);
         
-        if(response.statusCode() == 404) 
-            throw new NotFoundException(404, "Not found",null);
-
-        JsonObject convertedObject = new Gson().fromJson(response.body(), JsonObject.class);        
-        
-        return convertedObject;
+        String body = response.body().string();
+        response.body().close();
+        JsonObject convertedObject = new Gson().fromJson(body, JsonObject.class);
+        return convertedObject; 
     }
     
     public static JsonObject getTeamsFromApi(String league) throws IOException, InterruptedException, NotFoundException {
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-        .uri(URI.create(HOST+"/v2/competitions/"+league+"/teams"))
+        OkHttpClient client = new OkHttpClient();
+        String url = HOST+"/v2/competitions/"+league+"/teams";
+        Request request = new Request.Builder()
+        .url(url)
         .header("X-Auth-Token", API_KEY)
         .build();
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        Response response = client.newCall(request).execute();
         
-        if (response.statusCode() == 404)
+        if (response.code() == 404)
             throw new NotFoundException(404, "Not found", null);
-
-        JsonObject convertedObject = new Gson().fromJson(response.body(), JsonObject.class);
-
+        
+        String body = response.body().string();
+        response.body().close();
+        JsonObject convertedObject = new Gson().fromJson(body, JsonObject.class);
         return convertedObject;
 
     }
 
     public static JsonObject getTeamsPlayersFromAPI(int teamId) throws IOException, InterruptedException, NotFoundException {
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-        .uri(URI.create(HOST+"/v2/teams/"+teamId))
-        .header("X-Auth-Token", API_KEY).build();
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        
-        if (response.statusCode() == 404)
+        OkHttpClient client = new OkHttpClient();
+        String url = HOST+"/v2/teams/"+teamId;
+        Request request = new Request.Builder()
+        .url(url)
+        .header("X-Auth-Token", API_KEY)
+        .build();
+        Response response = client.newCall(request).execute();
+
+        if (response.code() == 404)
             throw new NotFoundException(404, "Not found", null);
 
         boolean success = false;
         while(!success) {
-            response = client.send(request, HttpResponse.BodyHandlers.ofString());
-            int status = response.statusCode();
+            response = client.newCall(request).execute();            
+            int status = response.code();    
             success = (status == 200);
-            if (!success) {
-                if(status == 429) {
+            if (!response.isSuccessful()) {
+                if(response.code() == 429) {
                     Thread.sleep(5000);
                 } else {
-                    throw new RuntimeException("Something went wrong: HTTP status: " + status);
+                    throw new RuntimeException("Something went wrong: HTTP status: " + response.code());
                 }
             }
         }
-        JsonObject convertedObject = new Gson().fromJson(response.body(), JsonObject.class);
+
+        String body = response.body().string();
+        response.body().close();
+        JsonObject convertedObject = new Gson().fromJson(body, JsonObject.class);
         return convertedObject;
     }
 }
